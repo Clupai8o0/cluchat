76d931bbb09ba2a5695bc36e3fe1831d7ae331d4
Clupai8o0 <clupaio4@gmail.com>
2024-09-06 22:21:21 +1000

    init proj

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { auth } from "./lib/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+import "./App.css";
+
+import NavBar from "./components/Navbar";
+import ChatBox from "./components/Chatbox";
+import Welcome from "./components/Welcome";
+
+function App() {
+  const [user] = useAuthState(auth);
+
+  return (
+    <div className="App">
+      <NavBar />
+      {!user ? (
+        <Welcome />
+      ) : (
+        <>
+          <ChatBox />
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
